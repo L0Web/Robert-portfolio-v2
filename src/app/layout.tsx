ac9f1160e3e513203e93c1fe7efcc4495a02b8ab
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-20 20:26:47 +0100

    suspended header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,10 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-200`}
         >
           <GlobalLoadingContextProvider>
-            <Header />
             <Suspense>
-              {children}
+              <Header />
             </Suspense>
+            {children}
             <Footer />
           </GlobalLoadingContextProvider>
         </body>
