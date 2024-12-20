6d4aab89972b5a0915b660162f399c25fef7057d
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-20 17:57:42 +0100

    Wrapped element in suspense boundary

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,6 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 
 const EASE = [.61,.41,.32,.96];
 
@@ -9,24 +10,26 @@ export default function Template({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
 
     return (
-        <AnimatePresence mode="wait">
-            <motion.div 
-                initial={{ scale: .9 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: .9 }} 
-                transition={{ duration: 1, ease: EASE }}
-                key={pathname}
-            >
+        <Suspense>
+            <AnimatePresence mode="wait">
                 <motion.div 
-                    key={pathname} 
-                    initial={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }} 
-                    animate={{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)' }} 
-                    exit={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+                    initial={{ scale: .9 }}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: .9 }} 
                     transition={{ duration: 1, ease: EASE }}
-                    className="fixed top-14 left-0 pointer-events-none bg-gray-200 w-screen h-screen z-[99999]" 
-                />
-                {children}
-            </motion.div>
-        </AnimatePresence>
+                    key={pathname}
+                >
+                    <motion.div 
+                        key={pathname} 
+                        initial={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }} 
+                        animate={{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)' }} 
+                        exit={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+                        transition={{ duration: 1, ease: EASE }}
+                        className="fixed top-14 left-0 pointer-events-none bg-gray-200 w-screen h-screen z-[99999]" 
+                    />
+                    {children}
+                </motion.div>
+            </AnimatePresence>
+        </Suspense>
     )
 }
