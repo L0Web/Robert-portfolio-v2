91dbdb8c50b92b7a337b6c2efd2c9661160c24ea
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-20 14:06:23 +0100

    Wrapped 'Artworks' component in 'Suspense' boundary

--- src/components/Artworks.tsx
+++ src/components/Artworks.tsx
@@ -32,7 +32,7 @@ const FETCH_QUERY = gql`
   }
 `;
 
-export default function ArtWorks() {
+function Artworks() {
   const limit = 10;
 
   const searchParams = useSearchParams();
@@ -115,27 +115,34 @@ export default function ArtWorks() {
   }, [data, prevArtworks, prevArtworkIds]);
   
   return (
-    <Suspense>
-      <FetchHandler 
-        isEmpty={Boolean(!data?.artworks?.length)} 
-        loading={loading} 
-        loadOnce={true} 
-        error={error} 
-        refetch={refetch}
+    <FetchHandler 
+      isEmpty={Boolean(!data?.artworks?.length)} 
+      loading={loading} 
+      loadOnce={true} 
+      error={error} 
+      refetch={refetch}
+    >
+      <ul
+        className="px-2 lg:px-4 mb-[64px] column-count-1 md:column-count-3"
       >
-        <ul
-          className="px-2 lg:px-4 mb-[64px] column-count-1 md:column-count-3"
-        >
-            {
-                artworks.map((artwork) => (
-                  artwork.images.length >= 1 ?
-                    <Artwork {...artwork} key={artwork.id} /> :
-                    null
-                ))
-            }
-        </ul>
-        {!loading ? <motion.span className="opacity-0" onViewportEnter={triggerNextPageFetch}></motion.span> : null}
-      </FetchHandler>
+          {
+              artworks.map((artwork) => (
+                artwork.images.length >= 1 ?
+                  <Artwork {...artwork} key={artwork.id} /> :
+                  null
+              ))
+          }
+      </ul>
+      {!loading ? <motion.span className="opacity-0" onViewportEnter={triggerNextPageFetch}></motion.span> : null}
+    </FetchHandler>
+  )
+};
+
+
+export default function ArtworksComponentExport() {
+  return (
+    <Suspense>
+      <Artworks />
     </Suspense>
   )
 }
