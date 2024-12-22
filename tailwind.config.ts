240e8e034c79c61328b85417c4d1a3c8b2cff7f4
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-22 12:28:00 +0100

    Added 'darkMode' field

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'selector',
   theme: {
     screens: {
       'sm': '400px',
