82fd8704eaf63102136cc470957e801607db6a6b
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-03-30 12:25:16 +0100

    Modified the layout of the component and fixed the animation logic

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -28,15 +28,26 @@ export default function Artwork({ _id, title, categories, images, isRelated }: M
         initial={{ opacity: 0 }} 
         animate={{ opacity: 1 }} 
         style={{ WebkitColumnBreakInside: 'avoid' } as React.CSSProperties} 
-        onClick={handleClick}
-        className="mb-2 w-full h-auto ease-expo transition-opaicity duration-500"
+        className="relative mb-2 w-full h-auto ease-expo transition-opaicity duration-500 flex flex-col"
       >
-        <Link 
-          href={`/artworks/artwork/${_id}`} 
-          onClick={(event) => (event.target as HTMLElement)?.classList?.contains('image-btn') && event.preventDefault()} 
-          className="flex flex-col gap-1"
-        >
-          <div className="relative">
+        <div className="relative">
+          {
+            images.length > 1 ? 
+              <>
+                <button onClick={prevImage} className={`image-btn ${currentImageIndex <= 0 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-opacity-100 active:opacity-80'} w-6 aspect-square flex items-center justify-center bg-white/50 opacity-80 rounded-full text-black backdrop-blur-md absolute top-1/2 -translate-y-1/2 left-3 z-[1]`}>
+                  <FiChevronLeft size={14} className="pointer-events-none" />
+                </button>
+                <button onClick={nextImage} className={`image-btn ${currentImageIndex >= images.length - 1 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-opacity-100 active:opacity-80'} w-6 aspect-square flex items-center justify-center bg-white/50 opacity-80 rounded-full text-black backdrop-blur-md absolute top-1/2 -translate-y-1/2 right-3 z-[1]`}>
+                  <FiChevronRight size={14} className="pointer-events-none" />
+                </button>
+              </> :
+              null
+          }
+          <Link 
+            href={`/artworks/artwork/${_id}`} 
+            onClick={handleClick}
+            className="flex flex-col gap-1"
+          >
             <div className="relative overflow-hidden rounded-xl">
               <Image 
                 src={images[0]?.image?.publicUrl} 
@@ -56,50 +67,34 @@ export default function Artwork({ _id, title, categories, images, isRelated }: M
                       <Image 
                         src={image.publicUrl} 
                         alt={`image of artwork: ${title} ${index + 2}`} 
-                        width={600} 
-                        height={1000} 
-                        className="w-full h-full object-cover" 
+                        fill
+                        className="object-cover" 
                       />
                     </div>
                   ))
                 }
               </div>
             </div>
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between p-2">
-              {
-                images.length > 1 ? 
-                  <>
-                  <button onClick={prevImage} className={`image-btn ${currentImageIndex <= 0 ? 'opacity-50 cursor-default' : 'hover:bg-opacity-100 active:opacity-80'} w-6 h-6 flex items-center justify-center bg-white/50 opacity-80 rounded-full text-black backdrop-blur-md`}>
-                    <FiChevronLeft size={14} className="pointer-events-none" />
-                  </button>
-                  <button onClick={nextImage} className={`image-btn ${currentImageIndex >= images.length - 1 ? 'opacity-50 cursor-default' : 'hover:bg-opacity-100 active:opacity-80'} w-6 h-6 flex items-center justify-center bg-white/50 opacity-80 rounded-full text-black backdrop-blur-md`}>
-                    <FiChevronRight size={14} className="pointer-events-none" />
-                  </button>
-                  </> :
-                  null
-              }
-            </div>
-          </div>
-            <div className="flex justify-between gap-4">
-                <div className="flex flex-col">
-                    <span className="text-xs lg:text-sm font-semibold tracking-tight dark:text-white/80">{title}</span>
-                    <span className="text-[.6rem] lg:text-xs tracking-tight text-gray-600 dark:text-white/50">
-                      {categories.slice(0,3).map(({ name }) => name).join(", ")}
-                      {categories.length > 3 ? <span>...</span> : null}
-                    </span>
-                </div>
-                <span className="flex items-center justify-center px-2 h-4 lg:h-6 rounded-full bg-gray-300 dark:bg-white/10 dark:text-white/60 text-[.55rem] lg:text-[.6rem]">
-                  {
-                    images.length > 1 ?
-                      <>
-                      <span>{currentImageIndex + 1}</span>
-                      <span>/</span>
-                      <span>{images.length}</span>
-                      </> :
-                      <span>Detail</span>
-                  }
+          </Link>
+        </div>
+        <Link href={`/artworks/artwork/${_id}`} onClick={handleClick} className="pt-2 flex justify-between gap-4">
+            <div className="flex flex-col">
+                <span className="text-xs lg:text-sm font-semibold tracking-tight dark:text-white/80">{title}</span>
+                <span className="text-[.6rem] lg:text-xs tracking-tight text-gray-600 dark:text-white/50">
+                  {categories}
                 </span>
             </div>
+            <span className="flex items-center justify-center px-2 h-4 lg:h-6 rounded-full bg-gray-300 dark:bg-white/10 dark:text-white/60 text-[.55rem] lg:text-[.6rem]">
+              {
+                images.length > 1 ?
+                  <>
+                  <span>{currentImageIndex + 1}</span>
+                  <span>/</span>
+                  <span>{images.length}</span>
+                  </> :
+                  <span>Detail</span>
+              }
+            </span>
         </Link>
       </motion.li>
     )
